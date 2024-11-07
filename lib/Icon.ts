bd3ee78bdf233f41af1bf60f0be4f0775a1f351c
abdelrahmanbaher1 <abdelrahmanbaher2@gmail.com>
2024-11-07 15:00:41 +0200

    fixing imports

--- lib/Icon.ts
+++ lib/Icon.ts
@@ -1,9 +1,9 @@
-import Moon from "@/public/icons/Moon.svg";
-import Sun from "@/public/icons/Sun.svg";
+import Moon from "@/public/icons/moon.svg";
+import Sun from "@/public/icons/sun.svg";
 import MagnifyingGlassIcon from "@/public/icons/search.svg";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline"; // Importing from Heroicons
 import ArrowLeft from "@/public/icons/ArrowLeft.svg";
-import ArrowRight from "@/public/icons/ArrowRight.svg";
+import ArrowRight from "@/public/icons/arrowRight.svg";
 
 export const ICONS = {
   Moon,
